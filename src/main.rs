3df0cc8ff443d9ede216ceba7f3953c7a0764cd4
Francesco Frassinelli <fraph24@gmail.com>
2017-07-24 00:04:40 +0200

    Bruteforce implemented

--- src/main.rs
+++ src/main.rs
@@ -53,17 +53,40 @@ fn next(table: &mut Vec<bool>, rows: usize, columns: usize) {
 fn main() {
     let rows = 4;
     let columns = 4;
-    let mut table = vec![
-        false,  true, false, false,
-        false, false,  true, false,
-         true,  true,  true, false,
-        false, false, false, false,
-    ];
-    assert!(rows*columns == table.len());
-    show(&table, rows, columns);
-    for _ in 0..5 {
-        println!();
-        next(&mut table, rows, columns);
-        show(&table, rows, columns);
-    }
+    let mut table = Vec::with_capacity(rows*columns);
+    for i in 0..(2u32.pow((rows*columns) as u32)) {
+		let s = format!("{:01$b}", i, rows*columns);
+		for j in 0..s.len() {
+		match s.chars().nth(j).unwrap() {
+			 '0' => table.push(false),
+			 '1' => table.push(true),
+			 _ => {},
+		 }
+		}
+		let mut history = Vec::new();
+		history.push(table.to_vec());
+		let mut repeated: i8;
+		loop {
+			repeated = -1;
+			next(&mut table, rows, columns);
+			'comparison: for i in 0..history.len() {
+				for j in 0..table.len() {
+					if history[i][j] != table[j]  {
+						continue 'comparison;
+					}
+				}
+				repeated = i as i8;
+				break;
+			}
+			if repeated >= 0 {
+				break;
+			}
+			history.push(table.to_vec());
+		}
+		if repeated == 0 {
+			println!("");
+			show(&table, rows, columns);
+		}
+		table.clear();
+	}
 }
